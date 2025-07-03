04aa3de942c2c34c1ce4c3b1123f868f1262ea38
adamp <adam.piskorek@gmx.de>
2025-07-03 18:58:41 +0200

    Content begrenzt auf 1920px

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-let photoArry = ['beach-1868342_1920.jpg', 'chiemsee-lake-8523044_1280.jpg', 'cliff-5954980_1280.jpg', 'cliff-7930586_1280.jpg', 'cliffs-9444605_1280.jpg', 'clouds-449822_1920.jpg', 'fog-7440132_1920.jpg', 'mountain-9631830_1920.jpg', 'sea-164989_1280.jpg', 'sea-205717_1920.jpg', 'sun-5685447_1920.jpg', 'sunset-3689760_1280.jpg', 'sunset-476589_1920.jpg', 'tree-736875_1280.jpg', 'trees-8482254_1280.jpg', 'trees-sunrise-3796183_1280.jpg', 'winter-2080070_1920.jpg'];
-
+let photoArry = [
+'beach-1868342_1920.jpg', 
+'chiemsee-lake-8523044_1280.jpg', 
+'cliff-5954980_1280.jpg', 
+'cliff-7930586_1280.jpg', 
+'cliffs-9444605_1280.jpg', 
+'clouds-449822_1920.jpg', 
+'fog-7440132_1920.jpg', 
+'mountain-9631830_1920.jpg', 
+'sea-164989_1280.jpg', 
+'sea-205717_1920.jpg', 
+'sun-5685447_1920.jpg', 
+'sunset-3689760_1280.jpg', 
+'sunset-476589_1920.jpg', 
+'tree-736875_1280.jpg', 
+'trees-8482254_1280.jpg', 
+'trees-sunrise-3796183_1280.jpg', 
+'winter-2080070_1920.jpg'];
 let content = '';
+let imageNameContainer = `<p id="imageNameContainerId">imageNameContainer</p>`;
+let buttons = `<div id="buttonsId">
+                    <img src="./img/Left.png" id="buttonLeft" class="buttons" onclick="clickLeft(event)"></button>
+                    <p id="imageCounter"></p>
+                    <img src="./img/Right.png" id="buttonRight" class="buttons" onclick="clickRight(event)"></button>
+               </div>`;
+let currentImageName='';
+
 function photoLoad() {
     for (i = 0; i < photoArry.length; i++) {
         content += `<img class="images" onclick="overlayOnOff(this.name)" src="./img/photo/${photoArry[i]}" alt="Photo-Dateiname: ${photoArry[i]}" name="${photoArry[i]}">`;
         document.getElementById('container').innerHTML = content;
     };
 };
-photoLoad();
-
-let imageNameContainer = `<p id="imageNameContainerId">imageNameContainer</p>`;
-
-let buttons = `<div id="buttonsId">
-            <img src="./img/Left.png" id="buttonLeft" class="buttons" onclick="clickLeft(event)"></button>
-            <p id="imageCounter"></p>
-            <img src="./img/Right.png" id="buttonRight" class="buttons" onclick="clickRight(event)"></button>
-            </div>`;
-let currentImageName='';
 
 function overlayOnOff(imageName) {
 
@@ -40,7 +54,6 @@ function overlayOnOff(imageName) {
     };
     currentImageName = imageName;
 }
-overlayOnOff();
 
 function clickLeft(event) {
     event.stopPropagation();
